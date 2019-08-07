d51407c3f826cc4bb08f7cf684f24b765d331e33
Marianne Pyon <mariannepyon@gmail.com>
2019-08-07 20:17:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_cost = 0
   lowest_item = nil
 
-  name_hash.collect do |item, cost|
+  name_hash.each do |item, cost|
     if lowest_cost == 0 || value < lowest_cost
       lowest_value = value
       lowest_item = item
